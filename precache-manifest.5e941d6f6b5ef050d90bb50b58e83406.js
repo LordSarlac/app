75ce8d0c45e5f3bae5cb1b604549044d6698d6f9
Wojtas <Wojtas@wp.plgit config --global user.email Wojtas@wp.plgit config --global user.email Wojtas@wp.pl>
2020-07-24 12:59:24 +0200

    Updates

---
+++ precache-manifest.5e941d6f6b5ef050d90bb50b58e83406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7580302c95f8775223b2970924f97a",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "1b04e3761e7d0df078d2",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10487b604660ad083bef",
+    "url": "/app/static/js/2.b1653fba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.b1653fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b04e3761e7d0df078d2",
+    "url": "/app/static/js/main.b7bd4245.chunk.js"
+  },
+  {
+    "revision": "f337bb034f4ca9807c8c",
+    "url": "/app/static/js/runtime-main.891824a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
